4ef117c24919bb61b20f549a4fd5f5c7e548eba8
jeweljose22 <117150116+jeweljose22@users.noreply.github.com>
2023-01-23 17:54:48 +0530

    Add files via upload

---
+++ pro3/pro3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("enter fare");
+double fare = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter time");
+double  time = Convert.ToDouble(Console.ReadLine());
+if (time >= 6 && time < 9)
+{
+    fare= fare * 1.1;
+    Console.WriteLine(fare);
+}
+else if (time >=9 && time < 17)
+{
+    fare = fare * 1.2;
+    Console.WriteLine(fare);
+}
+else if(time >=17 && time <=23)
+{
+    fare = fare * 1.07;
+    Console.WriteLine(fare);
+}
+else if ((time >=0 && time <= 6))
+{
+    fare = fare * 1.05;
+    Console.WriteLine(fare);
+}
+else
+{
+    Console.WriteLine(fare);
+}
+
